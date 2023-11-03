b0a55e6ba26335b16219fc23797fe9dcf39e582d
Constantine Zavezeon <31433211+Kwynto@users.noreply.github.com>
2023-11-03 17:37:24 +0300

    fix logger screen output

--- pkg/lib/prettylogger/prettylogger.go
+++ pkg/lib/prettylogger/prettylogger.go
@@ -56,15 +56,20 @@ func (h *PrettyHandler) Handle(ctx context.Context, r slog.Record) error {
 		return err
 	}
 
-	strAttrs := ""
+	strAttrsScreenOut := string(byteAttrs)
+	if strAttrsScreenOut == "{}" {
+		strAttrsScreenOut = ""
+	}
+
+	strAttrsFileOut := ""
 	switch h.env {
 	case EnvDev:
 		for k, v := range fields {
-			strAttrs = fmt.Sprintf("%s %s=\"%s\"", strAttrs, k, v.(string))
+			strAttrsFileOut = fmt.Sprintf("%s %s=\"%s\"", strAttrsFileOut, k, v.(string))
 		}
 	case EnvProd:
 		for k, v := range fields {
-			strAttrs = fmt.Sprintf("%s, \"%s\":\"%s\"", strAttrs, k, v.(string))
+			strAttrsFileOut = fmt.Sprintf("%s, \"%s\":\"%s\"", strAttrsFileOut, k, v.(string))
 		}
 	}
 
@@ -73,15 +78,15 @@ func (h *PrettyHandler) Handle(ctx context.Context, r slog.Record) error {
 	switch h.env {
 	case EnvDev:
 		timeStrFile := r.Time.Format("2006-01-02 15:04:05.000 -0700")
-		strFileOut = fmt.Sprintf("time=%s level=%v msg=\"%s\"%s", timeStrFile, r.Level, r.Message, strAttrs)
+		strFileOut = fmt.Sprintf("time=%s level=%v msg=\"%s\"%s", timeStrFile, r.Level, r.Message, strAttrsFileOut)
 	case EnvProd:
 		timeStrFile := r.Time.Format("2006-01-02T15:04:05.000000000-0700")
-		strFileOut = fmt.Sprintf("{\"time\":\"%s\",\"level\":\"%v\",\"msg\":\"%s\"%s}", timeStrFile, r.Level, r.Message, strAttrs)
+		strFileOut = fmt.Sprintf("{\"time\":\"%s\",\"level\":\"%v\",\"msg\":\"%s\"%s}", timeStrFile, r.Level, r.Message, strAttrsFileOut)
 	}
 
 	msg := color.CyanString(r.Message)
 
-	h.lScreen.Println(timeStrScreen, level, msg, color.WhiteString(string(byteAttrs)))
+	h.lScreen.Println(timeStrScreen, level, msg, color.WhiteString(strAttrsScreenOut))
 	h.lFile.Println(strFileOut)
 
 	return nil
