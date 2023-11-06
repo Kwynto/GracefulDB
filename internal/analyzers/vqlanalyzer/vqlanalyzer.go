3d487754e42c4d9820330adf0cdb14b05b7eec7b
Constantine Zavezeon <31433211+Kwynto@users.noreply.github.com>
2023-11-07 02:02:51 +0300

    minor fixes

--- internal/analyzers/vqlanalyzer/vqlanalyzer.go
+++ internal/analyzers/vqlanalyzer/vqlanalyzer.go
@@ -1,21 +1,14 @@
 package vqlanalyzer
 
-import (
-	"context"
-
-	"github.com/Kwynto/GracefulDB/internal/config"
-	"github.com/Kwynto/GracefulDB/pkg/lib/closer"
-)
-
 // TODO: Request
 func Request(instruction string) string {
 	return "There should be a response from the processed request."
 }
 
-func Analyzer(cfg *config.Config) {
-	// -
-}
+// func Analyzer(cfg *config.Config) {
+// 	// -
+// }
 
-func Shutdown(ctx context.Context, c *closer.Closer) {
-	c.Done()
-}
+// func Shutdown(ctx context.Context, c *closer.Closer) {
+// 	c.Done()
+// }
