fd9a021b667ec6e4ae8b53b082e447dc5bb123aa
Constantine Zavezeon <31433211+Kwynto@users.noreply.github.com>
2023-11-23 15:54:42 +0300

    gosession

--- internal/manage/webmanage/webmanage.go
+++ internal/manage/webmanage/webmanage.go
@@ -3,8 +3,11 @@ package webmanage
 import (
 	"context"
 	"fmt"
+	"html/template"
 	"log/slog"
 	"net/http"
+
+	"github.com/Kwynto/gosession"
 
 	"github.com/Kwynto/GracefulDB/internal/config"
 	"github.com/Kwynto/GracefulDB/pkg/lib/closer"
@@ -16,8 +19,50 @@ var muxWeb *http.ServeMux
 
 var srvWeb *http.Server
 
+func homeDefault(w http.ResponseWriter, r *http.Request, login string) {
+	ts, err := template.ParseFiles("./ui/html/home.html")
+	if err != nil {
+		slog.Debug("Internal Server Error", slog.String("err", err.Error()))
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		slog.Debug("Internal Server Error", slog.String("err", err.Error()))
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+}
+
+func homeAuth(w http.ResponseWriter, r *http.Request) {
+	ts, err := template.ParseFiles("./ui/html/auth.html")
+	if err != nil {
+		slog.Debug("Internal Server Error", slog.String("err", err.Error()))
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		slog.Debug("Internal Server Error", slog.String("err", err.Error()))
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+}
+
 func home(w http.ResponseWriter, r *http.Request) {
-	http.NotFound(w, r)
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	sesID := gosession.Start(&w, r)
+	auth := sesID.Get("auth")
+	if auth == nil {
+		homeAuth(w, r)
+	} else {
+		login := fmt.Sprint(auth)
+		homeDefault(w, r, login)
+	}
 }
 
 func routes() *http.ServeMux {
