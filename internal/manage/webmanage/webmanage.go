0616f13c86fbf5ead348336ec380d1c3edeb5995
Constantine Zavezeon <31433211+Kwynto@users.noreply.github.com>
2023-11-23 12:37:17 +0300

    Base for Web manager

--- internal/manage/webmanage/webmanage.go
+++ internal/manage/webmanage/webmanage.go
@@ -2,15 +2,52 @@ package webmanage
 
 import (
 	"context"
+	"fmt"
+	"log/slog"
+	"net/http"
 
 	"github.com/Kwynto/GracefulDB/internal/config"
 	"github.com/Kwynto/GracefulDB/pkg/lib/closer"
+	"github.com/Kwynto/GracefulDB/pkg/lib/prettylogger"
 )
 
+var address string
+var muxWeb *http.ServeMux
+
+var srvWeb *http.Server
+
+func home(w http.ResponseWriter, r *http.Request) {
+	http.NotFound(w, r)
+}
+
+func routes() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	return mux
+}
+
 func Start(cfg *config.Config) {
-	// -
+	address = fmt.Sprintf("%s:%s", cfg.WebServer.Address, cfg.WebServer.Port)
+	muxWeb = routes()
+
+	srvWeb = &http.Server{
+		Addr:     address,
+		ErrorLog: prettylogger.LogServerError,
+		Handler:  muxWeb,
+	}
+
+	slog.Info("Web manager is running", slog.String("address", address))
+	if err := srvWeb.ListenAndServe(); err != nil {
+		slog.Debug(err.Error())
+		return
+	}
 }
 
 func Shutdown(ctx context.Context, c *closer.Closer) {
+	if err := srvWeb.Shutdown(ctx); err != nil {
+		slog.Error("There was a problem with stopping the Web manager", slog.String("err", err.Error()))
+	}
+	slog.Info("Web manager stopped")
 	c.Done()
 }
