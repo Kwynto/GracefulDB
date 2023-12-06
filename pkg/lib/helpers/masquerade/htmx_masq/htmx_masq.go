610091fc2dc3143336d7e9948854de9a5f9c39ef
Constantine Zavezeon <31433211+Kwynto@users.noreply.github.com>
2023-12-06 23:58:01 +0300

    Final formating settings

--- pkg/lib/helpers/masquerade/htmx_masq/htmx_masq.go
+++ pkg/lib/helpers/masquerade/htmx_masq/htmx_masq.go
@@ -44,28 +44,38 @@ var Settings string = `
 <div class="container-fluid pt-4 px-4">
     <div class="row g-4">
         <div class="col-sm-6 col-xl-3">
-            <div class="bg-secondary rounded d-flex align-items-center justify-content-between p-4">
-                    <i class="fa fa-info fa-3x text-primary"></i><h5>Basic settings</h5>
-                    <div class="ms-3">
-                    
-                    <p class="mb-2">Environment: <h6 class="mb-0">{{.Env}}</h6></p>
-                    <hr>
-                    <p class="mb-2">LogPath: <h6 class="mb-0">{{.LogPath}}</h6></p>
-                    <hr>
-                    <p class="mb-2">ShutdownTimeOut: <h6 class="mb-0">{{.ShutdownTimeOut}}</h6></p>
-                    
-                    </div>
+            <div class="bg-secondary rounded p-4">
+                <h5 class="mb-4">Basic settings</h5>
+                <table class="table table-hover">
+                    <tbody>
+                        <tr>
+                            <td><h6>Environment: </h6></td>
+                            <td>{{.Env}}</td>
+                        </tr>
+                        <tr>
+                            <td><h6>LogPath: </h6></td>
+                            <td>{{.LogPath}}</td>
+                        </tr>
+                        <tr>
+                            <td><h6>ShutdownTimeOut: </h6></td>
+                            <td>{{.ShutdownTimeOut}}</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
 
         <div class="col-sm-6 col-xl-3">
-            <div class="bg-secondary rounded d-flex align-items-center justify-content-between p-4">
-                <i class="fa fa-tree fa-3x text-primary"></i><h5>Core settings</h5>
-                <div class="ms-3">
-
-                    <p class="mb-2">Bucket size: <h6 class="mb-0">{{.CoreSettings.BucketSize}}</h6></p>
-
-                </div>
+            <div class="bg-secondary rounded p-4">
+                <h5 class="mb-4">Core settings</h5>
+                <table class="table table-hover">
+                    <tbody>
+                        <tr>
+                            <td><h6>Bucket size: </h6></td>
+                            <td>{{.CoreSettings.BucketSize}}</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
@@ -74,66 +84,98 @@ var Settings string = `
 <div class="container-fluid pt-4 px-4">
     <div class="row g-4">
         <div class="col-sm-6 col-xl-3">
-            <div class="bg-secondary rounded d-flex align-items-center justify-content-between p-4">
-                <i class="fa fa-exchange-alt fa-3x text-primary"></i><h5>Web Socket</h5>
-                <div class="ms-3">
-                
-                <p class="mb-2">Enable: <h6 class="mb-0">{{.WebSocketConnector.Enable}}</h6></p>
-                <hr>
-                <p class="mb-2">Address: <h6 class="mb-0">{{.WebSocketConnector.Address}}</h6></p>
-                <hr>
-                <p class="mb-2">Port: <h6 class="mb-0">{{.WebSocketConnector.Port}}</h6></p>
-                <hr>
-                <p class="mb-2">BufferSize - Read: <h6 class="mb-0">{{.WebSocketConnector.BufferSize.Read}}</h6></p>
-                <hr>
-                <p class="mb-2">BufferSize - Write: <h6 class="mb-0">{{.WebSocketConnector.BufferSize.Write}}</h6></p>
-            
-                </div>
-            </div>
-        </div>
-    
-        <div class="col-sm-6 col-xl-3">
-            <div class="bg-secondary rounded d-flex align-items-center justify-content-between p-4">
-                <i class="fa fa-server fa-3x text-primary"></i><h5>REST</h5>
-                <div class="ms-3">
-                
-                <p class="mb-2">Enable: <h6 class="mb-0">{{.RestConnector.Enable}}</h6></p>
-                <hr>
-                <p class="mb-2">Address: <h6 class="mb-0">{{.RestConnector.Address}}</h6></p>
-                <hr>
-                <p class="mb-2">Port: <h6 class="mb-0">{{.RestConnector.Port}}</h6></p>
-                
-                </div>
+            <div class="bg-secondary rounded p-4">
+                <h5 class="mb-4">Web Socket Connector</h5>
+                <table class="table table-hover">
+                    <tbody>
+                        <tr>
+                            <td><h6>Enable: </h6></td>
+                            <td>{{.WebSocketConnector.Enable}}</td>
+                        </tr>
+                        <tr>
+                            <td><h6>Address: </h6></td>
+                            <td>{{.WebSocketConnector.Address}}</td>
+                        </tr>
+                        <tr>
+                            <td><h6>Port: </h6></td>
+                            <td>{{.WebSocketConnector.Port}}</td>
+                        </tr>
+                        <tr>
+                            <td><h6>BufferSize - Read: </h6></td>
+                            <td>{{.WebSocketConnector.BufferSize.Read}}</td>
+                        </tr>
+                        <tr>
+                            <td><h6>BufferSize - Write: </h6></td>
+                            <td>{{.WebSocketConnector.BufferSize.Write}}</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
 
         <div class="col-sm-6 col-xl-3">
-            <div class="bg-secondary rounded d-flex align-items-center justify-content-between p-4">
-                <i class="fa fa-shapes fa-3x text-primary"></i><h5>gRPC</h5>
-                <div class="ms-3">
-                
-                <p class="mb-2">Enable: <h6 class="mb-0">{{.GrpcConnector.Enable}}</h6></p>
-                <hr>
-                <p class="mb-2">Address: <h6 class="mb-0">{{.GrpcConnector.Address}}</h6></p>
-                <hr>
-                <p class="mb-2">Port: <h6 class="mb-0">{{.GrpcConnector.Port}}</h6></p>
-                
-                </div>
+            <div class="bg-secondary rounded p-4">
+                <h5 class="mb-4">REST Connector</h5>
+                <table class="table table-hover">
+                    <tbody>
+                        <tr>
+                            <td><h6>Enable: </h6></td>
+                            <td>{{.RestConnector.Enable}}</td>
+                        </tr>
+                        <tr>
+                            <td><h6>Address: </h6></td>
+                            <td>{{.RestConnector.Address}}</td>
+                        </tr>
+                        <tr>
+                            <td><h6>Port: </h6></td>
+                            <td>{{.RestConnector.Port}}</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
-    
+
         <div class="col-sm-6 col-xl-3">
-            <div class="bg-secondary rounded d-flex align-items-center justify-content-between p-4">
-                <i class="fa fa-window-restore fa-3x text-primary"></i><h5>Web Manager</h5>
-                <div class="ms-3">
-                
-                <p class="mb-2">Enable: <h6 class="mb-0">{{.WebServer.Enable}}</h6></p>
-                <hr>
-                <p class="mb-2">Address: <h6 class="mb-0">{{.WebServer.Address}}</h6></p>
-                <hr>
-                <p class="mb-2">Port: <h6 class="mb-0">{{.WebServer.Port}}</h6></p>
-                
-                </div>
+            <div class="bg-secondary rounded p-4">
+                <h5 class="mb-4">gRPC Connector</h5>
+                <table class="table table-hover">
+                    <tbody>
+                        <tr>
+                            <td><h6>Enable: </h6></td>
+                            <td>{{.GrpcConnector.Enable}}</td>
+                        </tr>
+                        <tr>
+                            <td><h6>Address: </h6></td>
+                            <td>{{.GrpcConnector.Address}}</td>
+                        </tr>
+                        <tr>
+                            <td><h6>Port: </h6></td>
+                            <td>{{.GrpcConnector.Port}}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        <div class="col-sm-6 col-xl-3">
+            <div class="bg-secondary rounded p-4">
+                <h5 class="mb-4">GracefulDB Web Manager</h5>
+                <table class="table table-hover">
+                    <tbody>
+                        <tr>
+                            <td><h6>Enable: </h6></td>
+                            <td>{{.WebServer.Enable}}</td>
+                        </tr>
+                        <tr>
+                            <td><h6>Address: </h6></td>
+                            <td>{{.WebServer.Address}}</td>
+                        </tr>
+                        <tr>
+                            <td><h6>Port: </h6></td>
+                            <td>{{.WebServer.Port}}</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     
