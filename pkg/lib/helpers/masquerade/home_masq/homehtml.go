094f2348efc41ae7cf77ef1b72715f688b7bc040
Constantine Zavezeon <31433211+Kwynto@users.noreply.github.com>
2023-12-18 01:59:46 +0300

    Changed modals

--- pkg/lib/helpers/masquerade/home_masq/homehtml.go
+++ pkg/lib/helpers/masquerade/home_masq/homehtml.go
@@ -146,6 +146,109 @@ var html3 string = `
         </div>
     </div>
 
+    <!-- Modals Accounts -->
+    <div class="modal fade" id="createModal" tabindex="-1" aria-labelledby="createModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+            <div class="modal-content bg-light" id="createModalSpace">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="createModalLabel">Create user</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body text-dark">
+                    <form id="create-user-form" hx-post="/hx/accounts/create_ok" hx-target="#createModalSpace" hx-trigger="submit">
+                        <div class="mb-3">
+                            <label for="login-input" class="col-form-label">Login:</label>
+                            <input type="text" class="form-control" name="login" id="login-input">
+                        </div>
+                        <div class="mb-3">
+                            <label for="password-input" class="col-form-label">Password:</label>
+                            <input type="password" class="form-control" name="password" id="password-input">
+                        </div>
+                        <div class="mb-3">
+                            <label for="desc-input" class="col-form-label">Description:</label>
+                            <input type="text" class="form-control" name="desc" id="desc-input">
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                    <button type="submit" form="create-user-form" class="btn btn-primary">Create</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="banModal" tabindex="-1" aria-labelledby="banModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+            <div class="modal-content bg-light" id="banModalSpace">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="banModalLabel">Ban user</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body text-dark">
+                    <form id="ban-user-form" hx-post="/hx/accounts/ban_ok" hx-target="#banModalSpace" hx-trigger="submit">
+                        <div class="mb-3">
+                            <label for="login-input" class="col-form-label">Login:</label>
+                            <input type="hidden" class="form-control" name="login" id="login-input" value="root">
+                        </div>
+                    </form>
+                    Block the root user?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                    <button type="submit" form="ban-user-form" class="btn btn-primary">Create</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="unbanModal" tabindex="-1" aria-labelledby="unbanModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+            <div class="modal-content bg-light" id="unbanModalSpace">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="unbanModalLabel">UnBlock user</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body text-dark">
+                    <form id="unban-user-form" hx-post="/hx/accounts/unban_ok" hx-target="#unbanModalSpace" hx-trigger="submit">
+                        <div class="mb-3">
+                            <input type="hidden" class="form-control" name="login" id="login-input" value="{{.Login}}">
+                        </div>
+                    </form>
+                    UnBlock the <b>{{.Login}}</b> user?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                    <button type="submit" form="unban-user-form" class="btn btn-primary">UnBlock</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="delModal" tabindex="-1" aria-labelledby="delModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+            <div class="modal-content bg-light" id="delModalSpace">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="delModalLabel">Delete user</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body text-dark">
+                    <form id="del-user-form" hx-post="/hx/accounts/del_ok" hx-target="#delModalSpace" hx-trigger="submit">
+                        <div class="mb-3">
+                            <label for="login-input" class="col-form-label">Login:</label>
+                            <input type="hidden" class="form-control" name="login" id="login-input" value="root">
+                        </div>
+                    </form>
+                    Delete the root user?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                    <button type="submit" form="del-user-form" class="btn btn-primary">Delete</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <!-- JavaScript Libraries -->
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
