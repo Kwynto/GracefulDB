65acddfe90238ed5cc36163b6024f72d3d9af0f2
Constantine Zavezeon <31433211+Kwynto@users.noreply.github.com>
2024-01-13 00:46:21 +0300

    Testing webmanager part 1

---
+++ internal/manage/webmanage/template_test.go
@@ -0,0 +1,40 @@
+package webmanage
+
+import (
+	"testing"
+
+	"github.com/Kwynto/GracefulDB/pkg/lib/helpers/masquerade/home_masq"
+)
+
+func Test_parseTemplates(t *testing.T) {
+	t.Run("parseTemplates() function testing", func(t *testing.T) {
+		parseTemplates() // calling the tested function
+
+		count := len(TemplatesMap)
+		if count == 0 {
+			t.Errorf("parseTemplates() error. Count: %d", count)
+		}
+	})
+}
+
+func Test_loadTemplateFromVar(t *testing.T) {
+	t.Run("loadTemplateFromVar() function testing - positive", func(t *testing.T) {
+		err := loadTemplateFromVar(HOME_TEMP_NAME, home_masq.HtmlHome) // calling the tested function
+		if err != nil {
+			t.Error("loadTemplateFromVar() error.")
+		}
+	})
+
+	t.Run("loadTemplateFromVar() function testing - negative", func(t *testing.T) {
+		wrongStr := `
+		<html>
+			{{ errorexp }}
+		</html>
+		`
+
+		err := loadTemplateFromVar("wrongStr", wrongStr) // calling the tested function
+		if err == nil {
+			t.Error("loadTemplateFromVar() error.")
+		}
+	})
+}
---
+++ internal/manage/webmanage/webmanage_test.go
@@ -0,0 +1,54 @@
+package webmanage
+
+import (
+	"context"
+	"net/http"
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/Kwynto/GracefulDB/internal/config"
+	"github.com/Kwynto/GracefulDB/pkg/lib/closer"
+)
+
+func Test_routes(t *testing.T) {
+	t.Run("routes() function testing", func(t *testing.T) {
+		res := routes() // calling the tested function
+
+		if reflect.TypeOf(res) != reflect.TypeOf(&http.ServeMux{}) {
+			t.Error("routes() error = The function returns the wrong type")
+		}
+	})
+}
+
+func Test_Start_and_Shutdown(t *testing.T) {
+	t.Run("Start() and Shutdown() function testing", func(t *testing.T) {
+		tf := "../../../../config/develop.yaml"
+		config.MustLoad(tf)
+		go Start(&config.DefaultConfig) // calling the tested function
+		closer.AddHandler(Shutdown)
+		time.Sleep(2 * time.Second)
+		// srvRest.Shutdown(context.Background())
+		Shutdown(context.Background(), closer.CloseProcs)
+
+		if reflect.TypeOf(muxWeb) != reflect.TypeOf(&http.ServeMux{}) {
+			t.Error("Start() error = The function has created an incorrect dependency.")
+		}
+
+		if reflect.TypeOf(srvWeb) != reflect.TypeOf(&http.Server{}) {
+			t.Error("Start() error = The function has created an incorrect dependency.")
+		}
+
+		if closer.CloseProcs.Counter != 0 {
+			t.Errorf("Shutdown() error: %v.", closer.CloseProcs.Counter)
+		}
+	})
+
+	t.Run("Shutdown() function testing - positive", func(t *testing.T) {
+		Shutdown(context.Background(), closer.CloseProcs)
+
+		if len(closer.CloseProcs.Msgs) > 0 {
+			t.Errorf("Shutdown() error.")
+		}
+	})
+}
