f56183cf5a51c10cac7a5efce4fe1175740e4f82
Constantine Zavezeon <31433211+Kwynto@users.noreply.github.com>
2024-01-18 21:33:07 +0300

    Prepeare

--- internal/engine/core/core.go
+++ internal/engine/core/core.go
@@ -3,6 +3,8 @@ package core
 import (
 	"context"
 	"log/slog"
+	"os"
+	"time"
 
 	"github.com/Kwynto/GracefulDB/internal/config"
 	"github.com/Kwynto/GracefulDB/pkg/lib/closer"
@@ -13,10 +15,21 @@ type tCoreSettings struct {
 	FreezeMode bool
 }
 
+type tCoreFile struct {
+	Descriptor *os.File
+	Expire     time.Duration
+}
+
+type tCoreProcessing struct {
+	FileDescriptors map[string]tCoreFile
+}
+
 var LocalCoreSettings tCoreSettings = tCoreSettings{
 	BucketSize: 800,
 	FreezeMode: false,
 }
+
+var CoreProcessing tCoreProcessing
 
 func LoadLocalCoreSettings(cfg *config.Config) tCoreSettings {
 	return tCoreSettings{
