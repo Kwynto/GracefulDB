733cfd160dde249c9073125b66fac67225685a23
Constantine Zavezeon <31433211+Kwynto@users.noreply.github.com>
2024-01-20 01:25:14 +0300

    Core begin

--- internal/engine/core/core.go
+++ internal/engine/core/core.go
@@ -2,6 +2,8 @@ package core
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"log/slog"
 	"os"
 	"time"
@@ -11,8 +13,14 @@ import (
 )
 
 type tCoreSettings struct {
+	Storage    string
 	BucketSize int
 	FreezeMode bool
+}
+
+type tDBInfo struct {
+	Name   string   `json:"name"`
+	Tables []string `json:"tables"`
 }
 
 type tCoreFile struct {
@@ -25,14 +33,50 @@ type tCoreProcessing struct {
 }
 
 var LocalCoreSettings tCoreSettings = tCoreSettings{
+	Storage:    "./data/",
 	BucketSize: 800,
 	FreezeMode: false,
 }
 
 var CoreProcessing tCoreProcessing
 
+func RemoveDB(name string) bool {
+	fullName := fmt.Sprintf("%s%s", LocalCoreSettings.Storage, name)
+	err := os.Remove(fullName)
+
+	return err == nil
+}
+
+func CreateDB(name string) bool {
+	fullName := fmt.Sprintf("%s%s", LocalCoreSettings.Storage, name)
+	err := os.Mkdir(fullName, 0666)
+	if err != nil {
+		return false
+	}
+
+	dbInfoPath := fmt.Sprintf("%s/%s", fullName, "info.json")
+	fo, err := os.OpenFile(dbInfoPath, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
+	if err != nil {
+		return false
+	}
+	defer fo.Close()
+
+	dbInfo := tDBInfo{
+		Name:   name,
+		Tables: []string{},
+	}
+	bytesDBInfo, err := json.Marshal(dbInfo)
+	if err != nil {
+		return false
+	}
+	fo.Write(bytesDBInfo)
+
+	return true
+}
+
 func LoadLocalCoreSettings(cfg *config.Config) tCoreSettings {
 	return tCoreSettings{
+		Storage:    cfg.CoreSettings.Storage,
 		BucketSize: cfg.CoreSettings.BucketSize,
 		FreezeMode: cfg.CoreSettings.FreezeMode,
 	}
