c0fbf5686a4aa21bf40cf37aa02159b96f8cfd6a
Constantine Zavezeon <31433211+Kwynto@users.noreply.github.com>
2024-02-05 21:48:22 +0300

    Correcting nonsense

--- internal/analyzers/sqlanalyzer/sqlanalyzer.go
+++ internal/analyzers/sqlanalyzer/sqlanalyzer.go
@@ -1,118 +1,103 @@
 package sqlanalyzer
 
 import (
+	"encoding/json"
 	"fmt"
 	"strings"
 	"unicode/utf8"
-
-	"github.com/Kwynto/GracefulDB/internal/engine/core"
-	"github.com/Kwynto/GracefulDB/pkg/lib/e"
 )
 
 type tQuery struct {
 	Instruction string
 	Placeholder []string
-	QueryLine   []string
+	// QueryLine   []string
 }
 
-func (q *tQuery) Decomposition() (err error) {
-	// -
-	op := "internal -> analyzers -> sql -> Decomposition"
-	defer func() { e.Wrapper(op, err) }()
+// func (q *tQuery) Decomposition() (err error) {
+// 	// -
+// 	op := "internal -> analyzers -> sql -> Decomposition"
+// 	defer func() { e.Wrapper(op, err) }()
 
-	nameExps := []string{
-		"SearchUse",
-		"SearchGrant",
-		"SearchRevoke",
-	}
+// 	var lastLen int = 0
+// 	for {
+// 		if curentLen := len(q.Instruction); curentLen <= 0 || curentLen == lastLen {
+// 			break
+// 		}
+// 		lastLen = len(q.Instruction)
+// 		q.SearchExp("AnyCommand")
+// 	}
 
-	var lastLen int = 0
+// 	return nil
+// }
 
-	for {
-		if curentLen := len(q.Instruction); curentLen <= 0 || curentLen == lastLen {
-			break
-		}
-		lastLen = len(q.Instruction)
-		q.SearchAllExp(nameExps)
-	}
+// func (q *tQuery) HeadCleaner() bool {
+// 	// This method is completes
+// 	re := core.RegExpCollection["HeadCleaner"]
 
-	return nil
-}
+// 	location := re.FindStringIndex(q.Instruction)
+// 	if len(location) > 0 && location[0] == 0 {
+// 		res := re.FindString(q.Instruction)
+// 		q.Instruction = strings.Replace(q.Instruction, res, "", 1)
+// 		return true
+// 	}
 
-func (q *tQuery) HeadCleaner() bool {
-	// This method is completes
+// 	return false
+// }
 
-	re := core.RegExpCollection["HeadCleaner"]
+// func (q *tQuery) SearchExp(nameExp string) bool {
+// 	// This method is complete
+// 	re := core.RegExpCollection[nameExp]
 
-	location := re.FindStringIndex(q.Instruction)
-	if len(location) > 0 && location[0] == 0 {
-		res := re.FindString(q.Instruction)
-		q.Instruction = strings.Replace(q.Instruction, res, "", 1)
-		return true
-	}
+// 	location := re.FindStringIndex(q.Instruction)
+// 	if len(location) > 0 && location[0] == 0 {
+// 		res := re.FindString(q.Instruction)
+// 		q.QueryLine = append(q.QueryLine, res)
+// 		q.Instruction = strings.Replace(q.Instruction, res, "", 1)
+// 		q.HeadCleaner()
+// 		return true
+// 	}
 
-	return false
-}
+// 	return false
+// }
 
-func (q *tQuery) SearchExp(nameExp string) bool {
-	// This method is complete
-	re := core.RegExpCollection[nameExp]
+// func (q *tQuery) SearchAllExp(nameExps []string) (err error) {
+// 	// This method is complete
+// 	op := "internal -> analyzers -> sql -> SearchAllExp"
+// 	defer func() { e.Wrapper(op, err) }()
 
-	location := re.FindStringIndex(q.Instruction)
-	if len(location) > 0 && location[0] == 0 {
-		res := re.FindString(q.Instruction)
-		q.QueryLine = append(q.QueryLine, res)
-		q.Instruction = strings.Replace(q.Instruction, res, "", 1)
-		q.HeadCleaner()
-		return true
-	}
+// 	for _, v := range nameExps {
+// 		if q.SearchExp(v) {
+// 			break
+// 		}
+// 	}
 
-	return false
-}
-
-func (q *tQuery) SearchAllExp(nameExps []string) (err error) {
-	// This method is complete
-	op := "internal -> analyzers -> sql -> SearchAllExp"
-	defer func() { e.Wrapper(op, err) }()
-
-	for _, v := range nameExps {
-		if q.SearchExp(v) {
-			break
-		}
-	}
-
-	return nil
-}
-
-func TailSign(inst string) string {
-	// This function is complete
-	r, _ := utf8.DecodeLastRuneInString(inst)
-	if r != ';' {
-		return fmt.Sprintf("%s;", inst)
-	}
-	return inst
-}
+// 	return nil
+// }
 
 // TODO: Request
 func Request(instruction *string, placeholder *[]string) *string {
 	// -
 	var res string
 
+	// Prep
 	inst := *instruction
+	// inst = core.RegExpCollection["LineBreak"].ReplaceAllLiteralString(inst, " ")
 	inst = strings.TrimSpace(inst)
-	inst = TailSign(inst)
+	if r, _ := utf8.DecodeLastRuneInString(inst); r != ';' {
+		inst = fmt.Sprintf("%s;", inst)
+	}
 
 	var query tQuery = tQuery{
 		Instruction: inst,
 		Placeholder: *placeholder,
-		QueryLine:   make([]string, 5),
+		// QueryLine:   make([]string, 5),
 	}
 
-	query.HeadCleaner()
+	// query.Decomposition()
 
-	query.Decomposition()
-
-	res = fmt.Sprint(query.QueryLine) // FIXME: Temporarily for tests
+	// res = fmt.Sprint(query.QueryLine) // FIXME: Temporarily for tests
+	bres, _ := json.Marshal(query)
+	res = string(bres)
 
 	return &res
 }
