1812b5b070b12abbe1dd8c21783267c2d7ca2156
Constantine Zavezeon <31433211+Kwynto@users.noreply.github.com>
2024-02-11 07:07:00 +0300

    Cleaning gtypes

--- internal/engine/basicsystem/gtypes/gtypes.go
+++ internal/engine/basicsystem/gtypes/gtypes.go
@@ -1,14 +1,10 @@
 package gtypes
 
-// type RowDB map[string]any
-
 type Secret struct {
 	Ticket   string `json:"ticket,omitempty"`
 	Login    string `json:"login,omitempty"`
 	Password string `json:"password,omitempty"`
 	Hash     string `json:"hash,omitempty"`
-	// QueryID  string `json:"queryid,omitempty"`
-	// Version  string `json:"version,omitempty"`
 }
 
 type Response struct {
@@ -16,27 +12,6 @@ type Response struct {
 	Ticket string `json:"ticket,omitempty"`
 	Result string `json:"result,omitempty"`
 }
-
-// type VData []RowDB
-
-// type VFields RowDB
-
-// type VQuery struct {
-// 	Action string  `json:"action"`
-// 	Secret VSecret `json:"secret"`
-// 	DB     string  `json:"db"`
-// 	Table  string  `json:"table"`
-// 	Fields VFields `json:"fields"`
-// 	Data   VData   `json:"data"`
-// }
-
-// type VAnswer struct {
-// 	Action      string  `json:"action"`
-// 	Secret      VSecret `json:"secret,omitempty"`
-// 	Data        VData   `json:"data,omitempty"`
-// 	Error       int     `json:"error"`
-// 	Description string  `json:"description,omitempty"`
-// }
 
 func DefaultSecret() Secret {
 	return Secret{}
