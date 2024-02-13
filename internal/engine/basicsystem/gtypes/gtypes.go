0c370028ac9c4bd79817a075c350bd7d68b3a49e
Constantine Zavezeon <31433211+Kwynto@users.noreply.github.com>
2024-02-13 23:35:23 +0300

    DCLUse complete

--- internal/engine/basicsystem/gtypes/gtypes.go
+++ internal/engine/basicsystem/gtypes/gtypes.go
@@ -13,6 +13,18 @@ type Response struct {
 	Result string `json:"result,omitempty"`
 }
 
+type TAccessFlags struct {
+	Create bool `json:"create,omitempty"`
+	Read   bool `json:"read,omitempty"`
+	Update bool `json:"update,omitempty"`
+	Delete bool `json:"delete,omitempty"`
+}
+
+type TAccess struct {
+	Owner string                  `json:"owner,omitempty"` // login
+	Flags map[string]TAccessFlags `json:"flags,omitempty"` // login - TAccessFlags
+}
+
 func DefaultSecret() Secret {
 	return Secret{}
 }
