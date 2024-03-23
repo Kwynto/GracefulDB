30f0827d774b01b4ca8a0cf532e786b26ce57125
Constantine Zavezeon <31433211+Kwynto@users.noreply.github.com>
2024-03-24 01:20:16 +0300

    New design - part 6

--- internal/manage/webmanage/template.go
+++ internal/manage/webmanage/template.go
@@ -6,7 +6,6 @@ import (
 	"log/slog"
 
 	"github.com/Kwynto/GracefulDB/pkg/lib/e"
-	"github.com/Kwynto/GracefulDB/pkg/lib/helpers/masquerade/htmx_masq"
 )
 
 const (
@@ -37,9 +36,9 @@ const (
 	BLOCK_TEMP_ACCOUNT_DEL_FORM_OK       = "ui/html/account-del-form-ok.html"
 	BLOCK_TEMP_ACCOUNT_DEL_FORM_LOAD     = "ui/html/account-del-form-load.html"
 	BLOCK_TEMP_ACCOUNT_DEL_FORM_ERROR    = "ui/html/account-del-form-error.html"
-	BLOCK_TEMP_ACCOUNT_SELFEDIT_OK       = "AccountSelfeditFormOk"
-	BLOCK_TEMP_ACCOUNT_SELFEDIT_LOAD     = "AccountSelfeditFormLoad"
-	BLOCK_TEMP_ACCOUNT_SELFEDIT_ERROR    = "AccountSelfeditFormError"
+	BLOCK_TEMP_ACCOUNT_SELFEDIT_OK       = "ui/html/selfedit-form-ok.html"
+	BLOCK_TEMP_ACCOUNT_SELFEDIT_LOAD     = "ui/html/selfedit-form-load.html"
+	BLOCK_TEMP_ACCOUNT_SELFEDIT_ERROR    = "ui/html/selfedit-form-error.html"
 
 	BLOCK_TEMP_SETTINGS = "ui/html/settings.html"
 )
@@ -90,66 +89,42 @@ func LoadTemplateFromEmbed(name string) (err error) {
 }
 
 func parseTemplates() {
-	// LoadTemplateFromString(HOME_TEMP_NAME, home_masq.HtmlHome)
 	LoadTemplateFromEmbed(HOME_TEMP_NAME)
-	// LoadTemplateFromString(AUTH_TEMP_NAME, auth_masq.HtmlAuth)
 	LoadTemplateFromEmbed(AUTH_TEMP_NAME)
 
-	// LoadTemplateFromString(BLOCK_TEMP_DEFAULT, htmx_masq.Default)
 	LoadTemplateFromEmbed(BLOCK_TEMP_DEFAULT)
-	// LoadTemplateFromString(BLOCK_TEMP_ACCESS_DENIED, htmx_masq.AccessDenied)
 	LoadTemplateFromEmbed(BLOCK_TEMP_ACCESS_DENIED)
 
-	// LoadTemplateFromString(BLOCK_TEMP_DASHBOARD, htmx_masq.Dashboard)
 	LoadTemplateFromEmbed(BLOCK_TEMP_DASHBOARD)
 
-	// LoadTemplateFromString(BLOCK_TEMP_DATABASES, htmx_masq.Databases)
 	LoadTemplateFromEmbed(BLOCK_TEMP_DATABASES)
-	// LoadTemplateFromString(BLOCK_TEMP_DATABASE_REQUEST_ANSWER, htmx_masq.DatabaseRequestAnswer)
 	LoadTemplateFromEmbed(BLOCK_TEMP_DATABASE_REQUEST_ANSWER)
 
-	// LoadTemplateFromString(BLOCK_TEMP_ACCOUNTS, htmx_masq.Accounts)
 	LoadTemplateFromEmbed(BLOCK_TEMP_ACCOUNTS)
 
-	// LoadTemplateFromString(BLOCK_TEMP_ACCOUNT_CREATE_FORM_OK, htmx_masq.AccountCreateFormOk)
 	LoadTemplateFromEmbed(BLOCK_TEMP_ACCOUNT_CREATE_FORM_OK)
-	// LoadTemplateFromString(BLOCK_TEMP_ACCOUNT_CREATE_FORM_LOAD, htmx_masq.AccountCreateFormLoad)
 	LoadTemplateFromEmbed(BLOCK_TEMP_ACCOUNT_CREATE_FORM_LOAD)
-	// LoadTemplateFromString(BLOCK_TEMP_ACCOUNT_CREATE_FORM_ERROR, htmx_masq.AccountCreateFormError)
 	LoadTemplateFromEmbed(BLOCK_TEMP_ACCOUNT_CREATE_FORM_ERROR)
 
-	// LoadTemplateFromString(BLOCK_TEMP_ACCOUNT_EDIT_FORM_OK, htmx_masq.AccountEditFormOk)
 	LoadTemplateFromEmbed(BLOCK_TEMP_ACCOUNT_EDIT_FORM_OK)
-	// LoadTemplateFromString(BLOCK_TEMP_ACCOUNT_EDIT_FORM_LOAD, htmx_masq.AccountEditFormLoad)
 	LoadTemplateFromEmbed(BLOCK_TEMP_ACCOUNT_EDIT_FORM_LOAD)
-	// LoadTemplateFromString(BLOCK_TEMP_ACCOUNT_EDIT_FORM_ERROR, htmx_masq.AccountEditFormError)
 	LoadTemplateFromEmbed(BLOCK_TEMP_ACCOUNT_EDIT_FORM_ERROR)
 
-	// LoadTemplateFromString(BLOCK_TEMP_ACCOUNT_BAN_FORM_OK, htmx_masq.AccountBanFormOk)
 	LoadTemplateFromEmbed(BLOCK_TEMP_ACCOUNT_BAN_FORM_OK)
-	// LoadTemplateFromString(BLOCK_TEMP_ACCOUNT_BAN_FORM_LOAD, htmx_masq.AccountBanFormLoad)
 	LoadTemplateFromEmbed(BLOCK_TEMP_ACCOUNT_BAN_FORM_LOAD)
-	// LoadTemplateFromString(BLOCK_TEMP_ACCOUNT_BAN_FORM_ERROR, htmx_masq.AccountBanFormError)
 	LoadTemplateFromEmbed(BLOCK_TEMP_ACCOUNT_BAN_FORM_ERROR)
 
-	// LoadTemplateFromString(BLOCK_TEMP_ACCOUNT_UNBAN_FORM_OK, htmx_masq.AccountUnBanFormOk)
 	LoadTemplateFromEmbed(BLOCK_TEMP_ACCOUNT_UNBAN_FORM_OK)
-	// LoadTemplateFromString(BLOCK_TEMP_ACCOUNT_UNBAN_FORM_LOAD, htmx_masq.AccountUnBanFormLoad)
 	LoadTemplateFromEmbed(BLOCK_TEMP_ACCOUNT_UNBAN_FORM_LOAD)
-	// LoadTemplateFromString(BLOCK_TEMP_ACCOUNT_UNBAN_FORM_ERROR, htmx_masq.AccountUnBanFormError)
 	LoadTemplateFromEmbed(BLOCK_TEMP_ACCOUNT_UNBAN_FORM_ERROR)
 
-	// LoadTemplateFromString(BLOCK_TEMP_ACCOUNT_DEL_FORM_OK, htmx_masq.AccountDelFormOk)
 	LoadTemplateFromEmbed(BLOCK_TEMP_ACCOUNT_DEL_FORM_OK)
-	// LoadTemplateFromString(BLOCK_TEMP_ACCOUNT_DEL_FORM_LOAD, htmx_masq.AccountDelFormLoad)
 	LoadTemplateFromEmbed(BLOCK_TEMP_ACCOUNT_DEL_FORM_LOAD)
-	// LoadTemplateFromString(BLOCK_TEMP_ACCOUNT_DEL_FORM_ERROR, htmx_masq.AccountDelFormError)
 	LoadTemplateFromEmbed(BLOCK_TEMP_ACCOUNT_DEL_FORM_ERROR)
 
-	LoadTemplateFromString(BLOCK_TEMP_ACCOUNT_SELFEDIT_OK, htmx_masq.SelfEditFormOk)
-	LoadTemplateFromString(BLOCK_TEMP_ACCOUNT_SELFEDIT_LOAD, htmx_masq.SelfEditFormLoad)
-	LoadTemplateFromString(BLOCK_TEMP_ACCOUNT_SELFEDIT_ERROR, htmx_masq.SelfEditFormError)
+	LoadTemplateFromEmbed(BLOCK_TEMP_ACCOUNT_SELFEDIT_OK)
+	LoadTemplateFromEmbed(BLOCK_TEMP_ACCOUNT_SELFEDIT_LOAD)
+	LoadTemplateFromEmbed(BLOCK_TEMP_ACCOUNT_SELFEDIT_ERROR)
 
-	// LoadTemplateFromString(BLOCK_TEMP_SETTINGS, htmx_masq.Settings)
 	LoadTemplateFromEmbed(BLOCK_TEMP_SETTINGS)
 }
--- ui/static/js/dashboard.js
+++ ui/static/js/dashboard.js
@@ -1,3 +1,4 @@
+// Background light
 const cursor = document.querySelector('#cursor');
 const onMouseChangePosition = (event) => {
     cursor.style.left = event.clientX + 'px';
@@ -18,6 +19,16 @@ htmx.on("#logoutModalCancel", "click", function(){
     htmx.removeClass(htmx.find("#logoutModal"), "showed");
     htmx.addClass(htmx.find('#logoutModal'), 'hidden');
 });
+
+// Selfedit (Profile) modal
+function showProfileModal() {
+    htmx.removeClass(htmx.find("#profileModal"), "hidden");
+    htmx.addClass(htmx.find("#profileModal"), "showed");
+}
+function hideProfileModal() {
+    htmx.removeClass(htmx.find("#profileModal"), "showed");
+    htmx.addClass(htmx.find("#profileModal"), 'hidden');
+}
 
 // Create modal
 function showCreateModal() {
@@ -68,77 +79,3 @@ function hideDelModal() {
     htmx.removeClass(htmx.find("#delModal"), "showed");
     htmx.addClass(htmx.find("#delModal"), 'hidden');
 }
-
-jQuery(document).ready(function(){
-    // $("#uBtnLogOut").click(function() {
-    //     $("#logoutModal").removeClass("hidden").addClass("showed");
-    // });
-    // $("#logoutModalBtnClose").click(function() {
-    //     $("#logoutModal").removeClass("showed").addClass("hidden");
-    // });
-    // $("#logoutModalCancel").click(function() {
-    //     $("#logoutModal").removeClass("showed").addClass("hidden");
-    // });
-
-    $("#uBtnYourProfile").click(function() {
-        $("#profileModal").removeClass("hidden").addClass("showed");
-    });
-    $("#profileModalBtnClose").click(function() {
-        $("#profileModal").removeClass("showed").addClass("hidden");
-    });
-    $("#profileModalCancel").click(function() {
-        $("#profileModal").removeClass("showed").addClass("hidden");
-    });
-
-    // $("#aBtnCreateAccount").click(function() {
-    //     $("#createModal").removeClass("hidden").addClass("showed");
-    // });
-    // $("#createModalBtnClose").click(function() {
-    //     $("#createModal").removeClass("showed").addClass("hidden");
-    // });
-    // $("#createModalCancel").click(function() {
-    //     $("#createModal").removeClass("showed").addClass("hidden");
-    // });
-
-    // $("#editModalBtnClose").click(function() {
-    //     $("#editModal").removeClass("showed").addClass("hidden");
-    // });
-    // $("#editModalCancel").click(function() {
-    //     $("#editModal").removeClass("showed").addClass("hidden");
-    // });
-
-    // $("#banModalBtnClose").click(function() {
-    //     $("#banModal").removeClass("showed").addClass("hidden");
-    // });
-    // $("#banModalCancel").click(function() {
-    //     $("#banModal").removeClass("showed").addClass("hidden");
-    // });
-    
-    // $("#unbanModalBtnClose").click(function() {
-    //     $("#unbanModal").removeClass("showed").addClass("hidden");
-    // });
-    // $("#unbanModalCancel").click(function() {
-    //     $("#unbanModal").removeClass("showed").addClass("hidden");
-    // });
-
-    // $("#delModalBtnClose").click(function() {
-    //     $("#delModal").removeClass("showed").addClass("hidden");
-    // });
-    // $("#delModalCancel").click(function() {
-    //     $("#delModal").removeClass("showed").addClass("hidden");
-    // });
-
-    // $(".trigger-edit").click(function() {
-    //     $("#editModal").removeClass("hidden").addClass("showed");
-    // });
-    // $(".trigger-ban").click(function() {
-    //     $("#banModal").removeClass("hidden").addClass("showed");
-    // });
-    // $(".trigger-unban").click(function() {
-    //     $("#unbanModal").removeClass("hidden").addClass("showed");
-    // });
-    // $(".trigger-del").click(function() {
-    //     $("#delModal").removeClass("hidden").addClass("showed");
-    // });
-
-  });
